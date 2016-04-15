189e925e1af3aaa5207c9498c1d8bd4fcc094b6d
Rylee Keys <rylee.k@gmail.com>
2016-04-15 10:33:47 -0600

    add octokit

--- app/controllers/users/authentications_controller.rb
+++ app/controllers/users/authentications_controller.rb
@@ -3,6 +3,18 @@ class Users::AuthenticationsController < ApplicationController
   def index
     redirect_to root_url if !current_user
     @apps = current_user.authentications
-    @app_providers = @apps.map { |auth| auth.authentication_provider.name }
+    @app_providers = {}
+    @apps.map do |auth|
+      @app_providers[auth.authentication_provider.name] = auth.authentication_provider.id
+    end
+    @github_handle = ""
+    @repo_names = []
+    if @app_providers.keys.include?("github")
+      gh_token = AuthenticationProvider.find(@app_providers["github"]).user_authentications.first.token
+      client = Octokit::Client.new(:access_token => gh_token)
+      @github_handle = client.user.login
+      @repo_names = client.repositories.map(&:name)
+    end
   end
+
 end
